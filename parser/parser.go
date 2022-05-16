6544e11ecc6cc8cccdcb2f17645e16af222e7e17
libra <23025996+smirror@users.noreply.github.com>
2022-05-16 22:34:41 +0900

    add ast & parser

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"monkey-go/ast"
+	"monkey-go/lexer"
+	"monkey-go/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	//2つトークンに読み込む。curTokenとpeekTokenの療法がセットされる。
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
