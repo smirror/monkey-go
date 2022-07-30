947e5977d73223a5e484bb1ee9f11408666556dc
libra <23025996+smirror@users.noreply.github.com>
2022-07-30 23:51:47 +0900

    add evaluator_test

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,44 @@
+package evaluator
+
+import (
+	"monkey-go/lexer"
+	"monkey-go/object"
+	"monkey-go/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, evaluated interface{}, expected int64) bool {
+	result, ok := evaluated.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", evaluated, evaluated)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
