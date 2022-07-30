f90cd7563baf7ecd9d08d32e32a86021eb62c20e
Y. Sato <23025996+smirror@users.noreply.github.com>
2022-07-30 23:36:14 +0900

    3.4.2: add object.Integer type (#16)
    add: object.Integer type

--- object/object.go
+++ object/object.go
@@ -1,8 +1,26 @@
 package object
 
+import "fmt"
+
 type ObjectType string
 
 type Object interface {
 	Type() ObjectType
 	Inspect() int
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
 }
