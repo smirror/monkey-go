dae404d4b5ba98fa80123e9421b64f5ce938387e
libra <23025996+smirror@users.noreply.github.com>
2022-08-04 00:34:05 +0900

    add: test cases

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -40,11 +40,11 @@ func TestReturnStatements(t *testing.T) {
 		input    string
 		expected int64
 	}{
-		//{"return 10;", 10},
-		//{"return 10; 9;", 10},
-		//{"return 2 * 5; 9;", 10},
-		//{"9; return 2 * 5; 9;", 10},
-		//{"if (10 > 1) { return 10; }", 10},
+		{"return 10;", 10},
+		{"return 10; 9;", 10},
+		{"return 2 * 5; 9;", 10},
+		{"9; return 2 * 5; 9;", 10},
+		{"if (10 > 1) { return 10; }", 10},
 		{
 			`
 		if (10 > 1) {
