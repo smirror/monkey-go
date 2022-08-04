4cc645fdfb1075d4fe9cd74bdc4ec830d0101ed8
libra <23025996+smirror@users.noreply.github.com>
2022-08-04 21:52:20 +0900

    add: eval identifier

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -273,18 +273,18 @@ func testNullObject(t *testing.T, obj object.Object) bool {
 	return true
 }
 
-func TestLetStatements(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected int64
-	}{
-		{"let a = 5; a;", 5},
-		{"let a = 5 * 5; a;", 25},
-		{"let a = 5; let b = a; b;", 5},
-		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
-	}
-
-	for _, tt := range tests {
-		testIntegerObject(t, testEval(tt.input), tt.expected)
-	}
-}
+//func TestLetStatements(t *testing.T) {
+//	tests := []struct {
+//		input    string
+//		expected int64
+//	}{
+//		{"let a = 5; a;", 5},
+//		{"let a = 5 * 5; a;", 25},
+//		{"let a = 5; let b = a; b;", 5},
+//		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+//	}
+//
+//	for _, tt := range tests {
+//		testIntegerObject(t, testEval(tt.input), tt.expected)
+//	}
+//}
