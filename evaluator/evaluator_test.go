6bec9e0168e18debb1985a963f832c771b53ff1d
libra <23025996+smirror@users.noreply.github.com>
2022-08-04 21:34:08 +0900

    add: let statement tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -129,10 +129,10 @@ if (10 > 1) {
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
-		//{
-		//	"foobar",
-		//	"identifier not found: foobar",
-		//},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -269,4 +269,20 @@ func testNullObject(t *testing.T, obj object.Object) bool {
 		return false
 	}
 	return true
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
 }
