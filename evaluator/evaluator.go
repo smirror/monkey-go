91074fcf34d733303112d1cb369ecc15ee06a549
libra <23025996+smirror@users.noreply.github.com>
2022-08-04 23:12:59 +0900

    add: extend function env

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -82,13 +82,48 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isError(function) {
 			return function
 		}
+
 		arg := evalExpressions(node.Arguments, env)
 		if len(arg) == 1 && isError(arg[0]) {
 			return arg[0]
 		}
+
+		return applyFunction(function, arg)
 	}
 
 	return nil
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
+}
+
+func extendFunctionEnv(
+	fn *object.Function,
+	args []object.Object,
+) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
 }
 
 func evalBangOperatorExpression(right object.Object) object.Object {
--- object/environment.go
+++ object/environment.go
@@ -2,6 +2,13 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
 
 func NewEnvironment() *Environment {
@@ -11,6 +18,9 @@ func NewEnvironment() *Environment {
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
