f315e21f035d5e9014a6b478a1f71e1ac0dbf62d
libra <23025996+smirror@users.noreply.github.com>
2023-02-23 12:14:39 +0900

    :art: add: token.STRING and lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -27,6 +27,8 @@ if (5<10){
 
 10 == 10;
 10 != 9;
+"foobar"
+"foo bar"
 `
 
 	tests := []struct {
@@ -114,6 +116,10 @@ if (5<10){
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
+
 		{token.EOF, ""},
 	}
 
