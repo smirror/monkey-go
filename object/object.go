67b10c15287ebcc7b833ade4aa899b350892d666
libra <23025996+smirror@users.noreply.github.com>
2023-02-23 17:18:44 +0900

    :art: add: len in builtin function

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -24,63 +25,40 @@ type Object interface {
 	Inspect() string
 }
 
+type BuiltinFunction func(args ...Object) Object
+
 type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Type() ObjectType {
-	return INTEGER_OBJ
-}
-
-func (i *Integer) Inspect() string {
-	return fmt.Sprintf("%d", i.Value)
-}
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) Type() ObjectType {
-	return BOOLEAN_OBJ
-}
-
-func (b *Boolean) Inspect() string {
-	return fmt.Sprintf("%t", b.Value)
-}
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
 
-func (n *Null) Type() ObjectType {
-	return NULL_OBJ
-}
-
-func (n *Null) Inspect() string {
-	return "null"
-}
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
 
 type ReturnValue struct {
 	Value Object
 }
 
-func (rv *ReturnValue) Type() ObjectType {
-	return RETURN_VALUE_OBJ
-}
-
-func (rv *ReturnValue) Inspect() string {
-	return rv.Value.Inspect()
-}
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 
 type Error struct {
 	Message string
 }
 
-func (e *Error) Type() ObjectType {
-	return ERROR_OBJ
-}
-
-func (e *Error) Inspect() string {
-	return "ERROR: " + e.Message
-}
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
 type Function struct {
 	Parameters []*ast.Identifier
@@ -88,10 +66,7 @@ type Function struct {
 	Env        *Environment
 }
 
-func (f *Function) Type() ObjectType {
-	return FUNCTION_OBJ
-}
-
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
 func (f *Function) Inspect() string {
 	var out bytes.Buffer
 
@@ -115,5 +90,11 @@ type String struct {
 }
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
 
-func (s *String) Inspect() string { return s.Value }
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
