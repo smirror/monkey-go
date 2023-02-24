ad5ace13077009f525c06106050fa83f5d70882c
libra <23025996+smirror@users.noreply.github.com>
2023-02-24 18:49:35 +0900

    :art: add: print

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey-go/object"
+import (
+	"fmt"
+	"monkey-go/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -100,6 +103,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"print": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
