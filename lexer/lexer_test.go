b58feaa9490f97ea3ce0846e632560e650e66588
libra <23025996+smirror@users.noreply.github.com>
2023-02-24 13:56:06 +0900

    :art: add: array in lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -29,6 +29,7 @@ if (5<10){
 10 != 9;
 "foobar"
 "foo bar"
+[1, 2];
 `
 
 	tests := []struct {
@@ -119,6 +120,13 @@ if (5<10){
 
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 
 		{token.EOF, ""},
 	}
